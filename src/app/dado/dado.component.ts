7b770d55dc83b6b752586208d18e77ed12b25b77
Alfonso <berni365@hotmail.com>
2020-01-15 14:21:16 +0100

    Juego para sacar 3 numeros aleatorios

---
+++ src/app/dado/dado.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dado',
+  templateUrl: './dado.component.html',
+  styleUrls: ['./dado.component.css']
+})
+export class DadoComponent implements OnInit {
+
+  constructor() { }
+  ngOnInit() {
+    {
+      return Math.floor(Math.random() * 6 + 1);
+    }
+  }
+
+}
